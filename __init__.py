4317cb6805b9df4b08e22270f107ccf030c411dd
Daniel Lewis <daniel.lewis@plailabs.com>
2024-04-04 15:17:53 -0600

    Patching

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 
 # Create /models/llm if it's not there
 import os
+
+from SaltAI.modules.node_importer import ModuleLoader
+
 import folder_paths
 
 Salt_LLM_DIR = os.path.join(folder_paths.models_dir, 'llm')
@@ -9,40 +12,27 @@ os.makedirs(Salt_LLM_DIR, exist_ok=True)
 # Set up JS
 WEB_DIRECTORY = "./web"
 
-# Core
-from .nodes_core import NODE_CLASS_MAPPINGS as nodes_core_classes
-from .nodes_core import NODE_DISPLAY_NAME_MAPPINGS as nodes_core_display_mappings
-NODE_CLASS_MAPPINGS = nodes_core_classes
-NODE_DISPLAY_NAME_MAPPINGS = nodes_core_display_mappings
+ROOT = os.path.abspath(os.path.dirname(__file__))
+NAME = "Salt.AI LLama-Index"
+PACKAGE = "SaltAI_LlamaIndex"
+NODES_DIR = os.path.join(ROOT, 'nodes')
+EXTENSION_WEB_DIRS = {}
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
 
-# Tools
-from .nodes_tools import NODE_CLASS_MAPPINGS as nodes_tools_classes
-from .nodes_tools import NODE_DISPLAY_NAME_MAPPINGS as nodes_tools_display_mappings
-NODE_CLASS_MAPPINGS.update(nodes_tools_classes)
-NODE_DISPLAY_NAME_MAPPINGS.update(nodes_tools_display_mappings)
+# Load modules
+module_timings = {}
+module_loader = ModuleLoader(PACKAGE)
+module_loader.load_modules(NODES_DIR)
 
-# LLMs
-from .llm import NODE_CLASS_MAPPINGS as llm_classes
-from .llm import NODE_DISPLAY_NAME_MAPPINGS as llm_display_mappings
-NODE_CLASS_MAPPINGS.update(llm_classes)
-NODE_DISPLAY_NAME_MAPPINGS.update(llm_display_mappings)
+# Mappings
+NODE_CLASS_MAPPINGS = module_loader.NODE_CLASS_MAPPINGS
+NODE_DISPLAY_NAME_MAPPINGS = module_loader.NODE_DISPLAY_NAME_MAPPINGS
 
-# Data
-from .data import NODE_CLASS_MAPPINGS as data_classes
-from .data import NODE_DISPLAY_NAME_MAPPINGS as data_display_mappings
-NODE_CLASS_MAPPINGS.update(data_classes)
-NODE_DISPLAY_NAME_MAPPINGS.update(data_display_mappings)
+# Timings and such
+print("")
+module_loader.report(NAME)
+print("")
 
-# Data Connectors
-from .data_connector import NODE_CLASS_MAPPINGS as data_connector_classes
-from .data_connector import NODE_DISPLAY_NAME_MAPPINGS as data__connector_display_mappings
-NODE_CLASS_MAPPINGS.update(data_connector_classes)
-NODE_DISPLAY_NAME_MAPPINGS.update(data__connector_display_mappings)
-
-# Query
-from .query import NODE_CLASS_MAPPINGS as query_classes
-from .query import NODE_DISPLAY_NAME_MAPPINGS as query_display_mappings
-NODE_CLASS_MAPPINGS.update(query_classes)
-NODE_DISPLAY_NAME_MAPPINGS.update(query_display_mappings)
-
+# Export nodes
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ modules/utility.py
@@ -0,0 +1,32 @@
+from typing import List, Dict, Any, Type, Tuple
+from pydantic import BaseModel, create_model
+import json
+
+
+class CreateOutputModel:
+    @staticmethod
+    def create(json_str: str, model_name: str) -> Type[BaseModel]:
+        input_dict = json.loads(json_str)
+        model_fields = {
+            key: CreateOutputModel.__get_type(key, value) for key, value in input_dict.items()
+        }
+        DynamicModel = create_model(model_name, **model_fields)
+        return DynamicModel
+
+    @staticmethod
+    def __get_type(key: str, value: Any) -> Tuple[Type, Ellipsis]:
+        if isinstance(value, list):
+            if value and all(isinstance(elem, str) for elem in value):
+                return (List[str], ...)
+            else:
+                return (List[Any], ...)
+        elif isinstance(value, dict):
+            return (Dict[str, Any], ...)
+        elif isinstance(value, str):
+            return (str, ...)
+        elif isinstance(value, int):
+            return (int, ...)
+        elif isinstance(value, float):
+            return (float, ...)
+        else:
+            return (Any, ...)
